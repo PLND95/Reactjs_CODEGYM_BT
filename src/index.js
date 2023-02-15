662ae2ac96421f4082dbd3c34a1bfba7833f5f93
PLND95 <phuclongmc@gmail.com>
2023-02-15 15:10:25 +0700

    .

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import Baitap1 from "./components/Baitap1";
 import Baitap2 from "./components/Baitap2";
 import Baitap3 from "./components/Baitap3";
+import Baitap4 from "./components/Baitap4";
+import "bootstrap/dist/css/bootstrap.css";
 
 
 
@@ -10,6 +12,6 @@ import Baitap3 from "./components/Baitap3";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-   <Baitap3/>
+   <Baitap4/>
 
 )
