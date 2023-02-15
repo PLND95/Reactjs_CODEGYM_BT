9c0ee7428d6bd5946a8a9bd2f8d142a971ac1ce7
PLND95 <phuclongmc@gmail.com>
2023-02-15 15:04:16 +0700

    .

---
+++ src/components/Baitap3.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import '../css/Baitap3.css'
+
+const Baitap3 = () => {
+  return (
+    <div>
+        <div className="container">
+            <div className="card">
+            <div className="card--header" />
+            <img
+                className="avatar"
+                src="https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                alt="avatar"
+            />
+            <div className="card--body">
+            <div>
+            <p className="text-header">Ruma Khan</p>
+            <p className="text-sub">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry
+            </p>
+            <button className="btn third">FOLLOW</button>
+            </div>
+            </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Baitap3
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Baitap1 from "./components/Baitap1";
 import Baitap2 from "./components/Baitap2";
+import Baitap3 from "./components/Baitap3";
 
 
 
@@ -9,6 +10,6 @@ import Baitap2 from "./components/Baitap2";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-   <Baitap2/>
+   <Baitap3/>
 
 )
