5daf84917b384acd68f69ee27a0a79ac9d9e43fc
PLND95 <phuclongmc@gmail.com>
2023-02-15 14:36:36 +0700

    .

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Baitap1 from "./components/Baitap1";
+import Baitap2 from "./components/Baitap2";
 
 
 
@@ -8,6 +9,6 @@ import Baitap1 from "./components/Baitap1";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-   <Baitap1/>
+   <Baitap2/>
 
 )
